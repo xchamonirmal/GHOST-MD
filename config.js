0c3a09b52ae25f1ecfb425da1691ba2eba395ef3
xchamonirmal <herokubot458@gmail.com>
2024-09-22 21:24:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,11 +5,11 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put season id",
+SESSION_ID: process.env.SESSION_ID || "EqFz2AwA#U2045cuSpC0UqpftUsLFA0V_5lutgTSbUT4nsxmVdI0",
 MONGODB: process.env.MONGODB || "mongodb://mongo:MfPJLADhRWNHvxuOORrlmfJHboedaADB@junction.proxy.rlwy.net:57828",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/397000a07a1deb7fad9c2.jpg",
-ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi GHOST-MD Is Online Now 💻\n*💻 Owner* - GHOST-MD\n\n*💻 Owner Number* -94741140620",
-SUDO_NB: process.env.SUDO_NB || "94741140620",
+ALIVE_MSG: process.env.ALIVE_IMG || "🦊 Hi CHAMO-MD Is Online Now 💻\n*💻 Owner* - CHAMO-MD\n\n*💻 Owner Number* -94743430452",
+SUDO_NB: process.env.SUDO_NB || "94743430452",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE:"true"
